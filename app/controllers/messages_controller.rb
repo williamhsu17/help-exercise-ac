d307275b8561071b6890d727357312981490195d
Wen-Tien Chang <ihower@gmail.com>
2015-06-30 10:02:46 +0800

    User can post message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,32 @@
 class MessagesController < ApplicationController
 
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def index
-    @messages = Message.page( params[:page] )
+    @messages = Message.order("id DESC").page( params[:page] )
   end
 
   def show
     @message = Message.find( params[:id] )
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new( message_params )
+    @message.user = current_user
+
+    @message.save!
+
+    redirect_to root_path
+  end
+
+  protected
+
+  def message_params
+    params.require(:message).permit(:title, :content, :category_name)
   end
 
 end
