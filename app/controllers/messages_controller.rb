d840f5e28175617579d9310bcf4a1b48348669df
williamhsu17 <williamhsu17@gmail.com>
2016-12-23 15:13:00 +0800

    2. 修 N+1 queries

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
 
   def index
     # TODO: fix N+1 queries for user and comments
-    @messages = Message.order("id DESC").page( params[:page] )
+    @messages = Message.includes(:user, :comments).order("id DESC").page( params[:page] )
 
     if params[:status] == "pending"
       # TODO: @messages = @messages.pending
