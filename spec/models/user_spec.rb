9664aea0afbb0991903802c7a6f993b3732dc346
williamhsu17 <williamhsu17@gmail.com>
2016-12-23 16:09:25 +0800

    5. (實作已經寫好，加單元測試)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,11 +25,20 @@ RSpec.describe User, type: :model do
   describe "#words_count" do
     before do
       # TODO: 加 Message 和 Comment 測試資料
+      other_user = User.create!( :email => "ooxx@example.org", :password => "12345678")
+      m1 = Message.create!( :user => other_user, :title => "1234", :content => "12 34")
+      Comment.create!( :user => other_user, :message => m1, :content => "12 34 56")
+
+      m2 = Message.create!( :user => @user, :title => "1234", :content => "1234")
+      Comment.create!( :user => @user, :message => m2, :content => "123 456")
+      Comment.create!( :user => @user, :message => m2, :content => "12 34 56")
     end
 
     it "加總該使用者的所有 Mesasge 和 Comment 的總字數" do
       # TODO: 測試 words_count 方法
-
+      other_user = User.find_by_email("ooxx@example.org")
+      expect( other_user.words_count ).to eq(6)
+      expect( @user.words_count ).to eq(7)
     end
   end
 
